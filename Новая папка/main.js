5e03cc86b46964d130a5398178961c4c711de395
NOK1A-33 <130181421+NOK1A-33@users.noreply.github.com>
2026-05-12 20:41:52 +0500

    Add files via upload

---
+++ Новая папка/main.js
@@ -0,0 +1,10 @@
+const clicker = document.querySelector(".click");
+const clekfd = document.querySelector("oncloicker");
+
+let nubmer = Number(0);
+ 
+function ckicker(){
+    nubmer++;
+    clekfd.textContent = nubmer;  
+}
+
